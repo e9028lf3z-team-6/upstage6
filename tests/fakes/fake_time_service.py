864cdc641f2c7c3d8a49598ccaeb78f0a0fdbdd7
Joens-codes <xkfmqhtkdn@hanmail.net>
2026-01-12 09:45:52 +0900

    삭제 버전

--- tests/fakes/fake_time_service.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class FakeTimeInfo:
-    hhmm: str
-
-class FakeTimeService:
-    def get_current_time(self, timezone: str):
-        return FakeTimeInfo(hhmm="13:30")
--- tests/test_agent_service.py
+++
@@ -1,7 +0,0 @@
-def test_agent_decision(agent_service):
-    response = agent_service.process_query(
-        "지금 런던 지사에 회의 요청해도 돼?"
-    )
-
-    assert "ai_message" in response
-    assert response["ai_message"] is not None
